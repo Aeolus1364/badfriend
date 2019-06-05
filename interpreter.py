40ad356d6e428f13147af031c04eaefb190ec6b3
Aeolus1364 <thetesseractsshadow@gmail.com>
2019-06-05 17:34:50 -0400

    commit

---
+++ interpreter.py
@@ -0,0 +1,13 @@
+class Interpreter:
+    def __init__(self):
+        command_pointer = 0
+        cell_pointer = 0
+
+        store = 0
+
+        data = []
+
+        commands = '<', '>', '?',
+
+    def load(self, code):
+
